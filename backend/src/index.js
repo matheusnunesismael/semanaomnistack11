6243e2f7f02bd0e6974bd80bf184b1afe2a2b970
matheusn <matheus.nunes_@hotmail.com>
2020-03-27 15:49:03 -0300

    dia 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes  = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());//fazer o express entender o formato json dentro da body
+app.use(cors());
 app.use(routes);
 
 app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,24 +1,30 @@
 const express = require('express');
+const crypto  = require('crypto');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController')
+
+
 const routes = express.Router();
 
+routes.post('/sessions', SessionController.create);
 
-/*
-======request :
-    ex. rota /teste
-.query  -> parametros no formato /teste?t=valor (pode incluir varios parametros como /teste?a=1&b=2&c=3)
-.params -> parametros no formato /teste/1, na declaração da rota deve incluir  o nome da variavel que irá receber ex.app.post('/teste/:id/:nome',... ->  params.params.id e params.params.nome 
-.body   -> parametros  em post
-*/
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create);
 
-routes.post('/teste', (request, response) => {
-    const teste = request.params.id;
+routes.get('/profile', ProfileController.index);
 
-    console.log(request.body);
-
-    return response.json({
-        nome : 'teste',
-        data : '12/23/545'
-    });
-});
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete)
 
 module.exports = routes;
+
+/*
+*======request :
+*  ex. rota /teste
+*    .query  -> parametros no formato /teste?t=valor (pode incluir varios parametros como /teste?a=1&b=2&c=3)
+*    .params -> parametros no formato /teste/1, na declaração da rota deve incluir  o nome da variavel que irá receber ex.app.post('/teste/:id/:nome',... ->  params.params.id e params.params.nome 
+*    .body   -> parametros  em post
+*/
